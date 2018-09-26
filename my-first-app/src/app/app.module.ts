fb9dfe3f7f3eadba0cf750cba620b0b6e5ef7569
Frank Edward Daza Gonzalez <frank.daza2@gmail.com>
2018-09-25 23:09:52 -0500

    Two way databinding.

--- my-first-app/src/app/app.module.ts
+++ my-first-app/src/app/app.module.ts
@@ -1,5 +1,6 @@
 import { BrowserModule } from '@angular/platform-browser';
 import { NgModule } from '@angular/core';
+import { FormsModule } from '@angular/forms';
 
 import { AppComponent } from './app.component';
 import { ServerComponent } from './components/server/server.component';
@@ -12,7 +13,8 @@ import { ServersComponent } from './components/servers/servers.component';
     ServersComponent
   ],
   imports: [
-    BrowserModule
+    BrowserModule,
+    FormsModule
   ],
   providers: [],
   bootstrap: [AppComponent]
