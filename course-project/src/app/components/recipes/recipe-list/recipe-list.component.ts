720247bd662039f3205d483104347fa36864410e
Frank Edward Daza Gonzalez <frank.daza2@gmail.com>
2018-10-01 22:58:35 -0500

    Outputting a list of recipes .

--- course-project/src/app/components/recipes/recipe-list/recipe-list.component.ts
+++ course-project/src/app/components/recipes/recipe-list/recipe-list.component.ts
@@ -11,6 +11,9 @@ export class RecipeListComponent implements OnInit {
   recipes: Recipe[] = [
     new Recipe('Plate of rice with corns and chicken',
     'delicious plate for vegetarians',
+    'https://imagesvc.timeincapp.com/v3/mm/image?url=https%3A%2F%2Fimg1.southernliving.timeinc.net%2Fsites%2Fdefault%2Ffiles%2Fstyles%2Fmedium_2x%2Fpublic%2Fimage%2F2018%2F02%2Fmain%2F2548601_qfsba_rice_with_scallops_152.jpg%3Fitok%3Dqvkz_lUq&w=1000&q=85'),
+    new Recipe('Plate of rice with corns and chicken',
+    'delicious plate for vegetarians',
     'https://imagesvc.timeincapp.com/v3/mm/image?url=https%3A%2F%2Fimg1.southernliving.timeinc.net%2Fsites%2Fdefault%2Ffiles%2Fstyles%2Fmedium_2x%2Fpublic%2Fimage%2F2018%2F02%2Fmain%2F2548601_qfsba_rice_with_scallops_152.jpg%3Fitok%3Dqvkz_lUq&w=1000&q=85')
   ];
 
