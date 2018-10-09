13761fac1419e16fe073d5ed00a1070398f254df
Frank Edward Daza González <frank.daza2@gmail.com>
2018-10-08 21:26:20 -0500

    Assigning an alias to custom properties

--- cmp-databinding-start/src/app/server-element/server-element.component.ts
+++ cmp-databinding-start/src/app/server-element/server-element.component.ts
@@ -7,7 +7,7 @@ import { Component, OnInit, Input } from '@angular/core';
 })
 export class ServerElementComponent implements OnInit {
 
-  @Input()
+  @Input('srvElement')
   element: {type: string, name: string, content: string};
   
 
