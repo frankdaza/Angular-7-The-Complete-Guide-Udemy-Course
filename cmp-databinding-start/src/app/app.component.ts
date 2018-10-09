944f8389f2a5c0a039ac2fc15647fa7239a4b88b
Frank Edward Daza González <frank.daza2@gmail.com>
2018-10-08 21:44:58 -0500

    Binding to custom events

--- cmp-databinding-start/src/app/app.component.ts
+++ cmp-databinding-start/src/app/app.component.ts
@@ -16,6 +16,20 @@ export class AppComponent {
   ngOnInit() {
   }
   
+  onServerAdded(serverData: {serverName: string, serverContent: string}) {
+    this.serverElements.push({
+      type: 'server',
+      name: serverData.serverName,
+      content: serverData.serverContent
+    });
+  }
 
+  onBlueprintAdded(blueprintData: { serverName: string, serverContent: string}) {
+    this.serverElements.push({
+      type: 'blueprint',
+      name: blueprintData.serverName,
+      content: blueprintData.serverContent
+    });
+  }
   
 }
