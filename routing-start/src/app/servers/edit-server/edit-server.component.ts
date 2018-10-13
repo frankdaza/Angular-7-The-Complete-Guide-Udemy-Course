c5b1d6dd9c8f32ea4873875a31912e625f2aa45c
Frank Edward Daza González <knarf.developer@gmail.com>
2018-10-13 14:57:06 -0500

    Retrieving query parameters and fragments.

--- routing-start/src/app/servers/edit-server/edit-server.component.ts
+++ routing-start/src/app/servers/edit-server/edit-server.component.ts
@@ -1,23 +1,52 @@
-import { Component, OnInit } from '@angular/core';
+import { Component, OnInit, OnDestroy } from '@angular/core';
 
 import { ServersService } from '../servers.service';
+import { ActivatedRoute } from '@angular/router';
+import { Subscription } from 'rxjs';
 
 @Component({
   selector: 'app-edit-server',
   templateUrl: './edit-server.component.html',
   styleUrls: ['./edit-server.component.css']
 })
-export class EditServerComponent implements OnInit {
-  server: {id: number, name: string, status: string};
-  serverName = '';
-  serverStatus = '';
+export class EditServerComponent implements OnInit, OnDestroy {
+  
+  private server: {id: number, name: string, status: string};
+  private serverName = '';
+  private serverStatus = '';
+  private subscription: Subscription;
 
-  constructor(private serversService: ServersService) { }
+  constructor(
+    private serversService: ServersService,
+    private activedRoute: ActivatedRoute
+  ) { }
 
   ngOnInit() {
+    this.subscription = this.activedRoute.params.subscribe(
+      params => {
+        console.log(params['id']);
+      }
+    );
+
+    this.subscription = this.activedRoute.queryParams.subscribe(
+      params => {
+        console.log(params);
+      }
+    );
+
+    this.subscription = this.activedRoute.fragment.subscribe(
+      params => {
+        console.log(params);
+      }
+    );
+
     this.server = this.serversService.getServer(1);
     this.serverName = this.server.name;
     this.serverStatus = this.server.status;
+  }
+
+  ngOnDestroy() {
+    this.subscription.unsubscribe();
   }
 
   onUpdateServer() {
