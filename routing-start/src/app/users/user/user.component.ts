86f40f350cec8b88bdc2dfd63f4f2fc035231f98
Frank Edward Daza González <knarf.developer@gmail.com>
2018-10-13 14:34:06 -0500

    Unsubscribe.

--- routing-start/src/app/users/user/user.component.ts
+++ routing-start/src/app/users/user/user.component.ts
@@ -1,26 +1,32 @@
-import { Component, OnInit } from '@angular/core';
+import { Component, OnInit, OnDestroy } from '@angular/core';
 import { ActivatedRoute } from '@angular/router';
+import { Subscription } from 'rxjs';
 
 @Component({
   selector: 'app-user',
   templateUrl: './user.component.html',
   styleUrls: ['./user.component.css']
 })
-export class UserComponent implements OnInit {
-  user: {id: number, name: string};
+export class UserComponent implements OnInit, OnDestroy {
+  
+  private user: {id: number, name: string};
+  private subscription: Subscription;
 
   constructor(
     private router: ActivatedRoute
   ) { }
 
   ngOnInit() {
-    this.router.params.subscribe(params => {
+    this.subscription = this.router.params.subscribe(params => {
       this.user = {
         id: params['id'],
         name: params['name']
       }
     });
-    
+  }
+
+  ngOnDestroy() {
+    this.subscription.unsubscribe();
   }
 
 }
