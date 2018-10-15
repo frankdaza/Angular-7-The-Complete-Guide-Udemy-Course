6e48d851136fb4bac81c43c5871eba90f1a13d7c
Frank Edward Daza González <knarf.developer@gmail.com>
2018-10-15 15:01:08 -0500

    TD: Submitting and using the form.

--- forms-td-start/src/app/app.component.ts
+++ forms-td-start/src/app/app.component.ts
@@ -1,4 +1,5 @@
 import { Component } from '@angular/core';
+import { NgForm } from '@angular/forms';
 
 @Component({
   selector: 'app-root',
@@ -6,7 +7,14 @@ import { Component } from '@angular/core';
   styleUrls: ['./app.component.css']
 })
 export class AppComponent {
+  
   suggestUserName() {
     const suggestedName = 'Superuser';
   }
+
+  onSubmit(form: NgForm) {
+    console.log(form.valid);
+    console.log(form.value);
+  }
+
 }
