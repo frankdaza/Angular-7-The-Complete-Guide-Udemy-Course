2e27a442dac57d534abacfec70b92e5db569fed9
Frank Edward Daza González <knarf.developer@gmail.com>
2018-10-16 22:50:38 -0500

    Understanding the async pipe.

--- pipes-start/src/app/app.component.ts
+++ pipes-start/src/app/app.component.ts
@@ -8,6 +8,11 @@ import { Component } from '@angular/core';
 export class AppComponent {
 
   filteredStatus = '';
+  appStatus = new Promise((resolve, reject) => {
+    setTimeout(() => {
+      resolve('stable');
+    }, 2000);
+  });
 
   servers = [
     {
