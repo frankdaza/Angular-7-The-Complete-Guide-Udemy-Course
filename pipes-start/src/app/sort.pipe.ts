e77fff15f99d7ce70faaf9ae17106ccf2c097228
Frank Edward Daza González <knarf.developer@gmail.com>
2018-10-16 22:58:58 -0500

    Finished assignment 8.

---
+++ pipes-start/src/app/sort.pipe.ts
@@ -0,0 +1,18 @@
+import { Pipe, PipeTransform } from '@angular/core';
+
+@Pipe({
+  name: 'sort'
+})
+export class SortPipe implements PipeTransform {
+
+  transform(value: any, propName: string): any {
+    return value.sort((a, b) => {
+      if (a[propName] > b[propName]) {
+        return 1;
+      } else {
+        return -1;
+      }
+    });
+  }
+
+}
