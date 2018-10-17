610cde44418f5b5dce8892c96b601b45ea142111
Frank Edward Daza González <knarf.developer@gmail.com>
2018-10-16 22:17:21 -0500

    Creating a custom pipe.

---
+++ pipes-start/src/app/shorten.pipe.ts
@@ -0,0 +1,14 @@
+import { PipeTransform, Pipe } from "@angular/core";
+
+@Pipe({ name: 'shorten' })
+export class ShortenPipe implements PipeTransform {
+
+  transform(value: any) {
+    if (value.length >  10) {
+      return value.substr(0, 10) + ' ...';
+    }
+
+    return value;
+  }
+
+}
