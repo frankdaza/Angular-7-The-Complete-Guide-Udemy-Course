6c4858b17a9fdea0c4eaf60bc516def3a79b923c
Frank Edward Daza González <frank.daza2@gmail.com>
2019-02-02 10:53:05 -0500

    Registering the Application Store

--- 01-ngrx-basics-start/src/app/app.module.ts
+++ 01-ngrx-basics-start/src/app/app.module.ts
@@ -8,6 +8,8 @@ import { SharedModule } from './shared/shared.module';
 import { ShoppingListModule } from './shopping-list/shopping-list.module';
 import { AuthModule } from './auth/auth.module'
 import { CoreModule } from './core/core.module';
+import { StoreModule } from '@ngrx/store';
+import { ShoppingListReducer } from './shopping-list/shopping-list.reducers';
 
 @NgModule({
   declarations: [
@@ -20,7 +22,8 @@ import { CoreModule } from './core/core.module';
     SharedModule,
     ShoppingListModule,
     AuthModule,
-    CoreModule
+    CoreModule,
+    StoreModule.forRoot({ shoppingList: ShoppingListReducer })
   ],
   bootstrap: [AppComponent]
 })
--- 01-ngrx-basics-start/src/app/shopping-list/shopping-list.reducers.ts
+++ 01-ngrx-basics-start/src/app/shopping-list/shopping-list.reducers.ts
@@ -1,4 +1,4 @@
-import * as ShoppinListActions from './shopping-list.actions';
+import * as ShoppingListActions from './shopping-list.actions';
 import { Ingredient } from '../shared/ingredient.model';
 
 export const ADD_INGREDIENT = "ADD_INGREDIENT";
@@ -10,14 +10,14 @@ const initialState = {
 	]
 };
 
-export function shoppinListReducer(state = initialState, action: ShoppinListActions.ShoppingListActions) {
+export function ShoppingListReducer(state = initialState, action: ShoppingListActions.ShoppingListActions) {
 	switch (action.type) {
-		case ShoppinListActions.ADD_INGREDIENT:
+		case ShoppingListActions.ADD_INGREDIENT:
 			return {
 				...state,
 				ingredients: [...state.ingredients, action.payload]
 			};
-			
+
 			default:
 				return state;
 	}
